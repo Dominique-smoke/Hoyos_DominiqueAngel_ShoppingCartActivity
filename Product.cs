e5eda4b3d833ec93b0ae4459210cc725c1a24cbe
Dominique-smoke <pengwingforger@gmail.com>
2026-04-11 21:24:19 +0800

    hello ebriwan

---
+++ Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hoyos_ShoppingCartActivity
+{
+    internal class Product
+       
+    {
+        //erm tama ba to okay na ba to
+    }
+}
